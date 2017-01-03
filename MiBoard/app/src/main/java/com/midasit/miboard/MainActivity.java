763c82dd4dd698b43f97a4e7993c33c8989cd90b
ParkJiyoung <realplay23@naver.com>
2017-01-03 20:29:59 +0900

    프로젝트 생성

---
+++ MiBoard/app/src/main/java/com/midasit/miboard/MainActivity.java
@@ -0,0 +1,25 @@
+package com.midasit.miboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void initModel() {
+
+    }
+
+    private void initView() {
+
+    }
+
+    private void initListener() {
+
+    }
+}
