b587fc355320140cce5ebaac4ac178756aa3f18e
ParkJiyoung <realplay23@naver.com>
2017-01-05 19:21:15 +0900

    Data 전송을 위한 Model 담는 class 생성.

---
+++ MiBoard/app/src/main/java/com/midasit/miboard/MiboardData.java
@@ -0,0 +1,43 @@
+package com.midasit.miboard;
+
+/**
+ * Created by JiyoungPark on 2017. 1. 5..
+ */
+public class MiboardData {
+    String id, title, content, imageName;
+
+    public MiboardData() {
+        this.id = UserInfo.getInstance().id;
+        this.title = null;
+        this.content = null;
+        this.imageName = null;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+}
